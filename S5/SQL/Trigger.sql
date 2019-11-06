533189775b1955d27c31cf71f2e73276c01fdc1d
Joann Mary Varghese <40649303+vibrantlyboho@users.noreply.github.com>
2019-11-06 13:47:54 +0530

    Create Trigger.sql

---
+++ S5/SQL/Trigger.sql
@@ -0,0 +1,6 @@
+emp(id, name, )
+
+empbackup(empid, name, dob, joindate, salary, deptid, dateofoperation, typeofoperation)
+incometax(empid, name, dob, joindate, salary, deptid, taxamount)
+
+Write a trigger which converts employee name into uppercase if it is entered in lowercase
