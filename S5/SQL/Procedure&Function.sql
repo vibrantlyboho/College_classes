2cda7a82c9fe2fc681b77ae7c8b10ce7d70c6734
Joann Mary Varghese <40649303+vibrantlyboho@users.noreply.github.com>
2019-11-06 14:11:04 +0530

    Update Procedure&Function.sql

--- S5/SQL/Procedure&Function.sql
+++ S5/SQL/Procedure&Function.sql
@@ -2,21 +2,19 @@
 
 create table department
 (deptid integer PRIMARY KEY,
- deptname varchar(20),
+ deptname varchar(20)
  );
 
 /*employee(eid, ename, dob, joindate, salary, deptid)*/
 
-create table employee
+create table employeetable
 (eid integer PRIMARY KEY,
  ename varchar(20),
  dob date,
  joindate date,
  salary integer,
- deptid integer
- 
- FOREIGN KEY(deptid) references department(deptid));
-
+ deptid integer, 
+ foreign key(deptid) references department(deptid));
 
 /*Write a function or procedure for updating the salary of employees working in the dept with dept id=10 by 20%
 Write a function or procedure for employee which accept dept number & returns the highest salary in the dept*/
