c476953527d877b79a94f8f5d555b0dbc0b4b29f
Joann Mary Varghese <40649303+vibrantlyboho@users.noreply.github.com>
2019-11-06 13:38:32 +0530

    Create Procedure&Function.sql

---
+++ S5/SQL/Procedure&Function.sql
@@ -0,0 +1,7 @@
+employee(id, name, dob, joindate, salary, deptid)
+
+department(deptid, deptname)
+
+
+Write a function or procedure for updating the salary of employees working in the dept with dept id=10 by 20%
+Write a function or procedure for employee which accept dept number & returns the highest salary in the dept 
