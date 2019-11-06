3d0c6160fd950271f40b58f3c27d9a4dc71e533b
Joann Mary Varghese <40649303+vibrantlyboho@users.noreply.github.com>
2019-11-06 13:50:31 +0530

    Update Trigger.sql

--- S5/SQL/Trigger.sql
+++ S5/SQL/Trigger.sql
@@ -8,3 +8,5 @@ incometax(empid, name, dob, joindate, salary, deptid, taxamount)
 
 
 Write a trigger which converts employee name into uppercase if it is entered in lowercase
+Write a trigger that stores the data of employee in employeebackup for every delete operation and 
+stores the old data for every update operation
